14287c113e6660d1381c9ad9ea2ad0b151ad928b
skypaw <53307746+skypaw@users.noreply.github.com>
2023-10-24 17:20:24 +0200

    Add articles every monday (#40)
    * Add article with jekyll
    
    * add config
    
    * Move config
    
    * Fix workflow path
    
    * Add jekyll gem
    
    * Add jekyll gem
    
    * Add only built artifacts
    
    * Remove layout
    
    * Add variable
    
    * Add html content
    
    * Add liquid tag
    
    * add closest sunday article
    
    * Add default layout
    
    * Add space after title
    
    * Remove gemfiles
    
    * Refactor sites
    
    * Format article sites urls
    
    * Add liquid code to index
    
    * Add formatting to date
    
    * Add iteration from data
    
    * Add favicon
    
    * Change additon
    
    * Refactor iteration
    
    * Assing i in for loop
    
    * Assing i in for loop
    
    * Assing i in for loop
    
    * Assing i in for loop
    
    * Add python script for filtering
    
    * add logic to filter

--- filter_data.py
+++ filter_data.py
@@ -1,3 +1,20 @@
-with open('./_data/shopping-sundays') as file:
-    for date in file.readlines():
-        print(date)
+from datetime import datetime
+import csv
+
+
+def remove_past_sundays(date):
+    sunday = datetime.fromisoformat(date)
+    now = datetime.now()
+    if sunday > now:
+        return True
+    else:
+        return False
+
+
+filtered_dates = []
+with open('./_data/shopping-sundays.csv', 'r') as file:
+    for dates in csv.reader(file):
+        filtered_dates = filter(remove_past_sundays, dates)
+
+with open('./_data/filtered-shopping-sundays.csv', 'w') as file:
+    csv.writer(file).writerow(filtered_dates)
