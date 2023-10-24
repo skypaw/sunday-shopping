b65056b210243cb53eb6da0c69af6ce63fc6a241
skypaw <53307746+skypaw@users.noreply.github.com>
2023-10-24 17:50:58 +0200

    Add articles every monday (#42)
    * Add article with jekyll
    
    * add config
    
    * Move config
    
    * Fix workflow path
    
    * Add jekyll gem
    
    * Add jekyll gem
    
    * Add only built artifacts
    
    * Remove layout
    
    * Add variable
    
    * Add html content
    
    * Add liquid tag
    
    * add closest sunday article
    
    * Add default layout
    
    * Add space after title
    
    * Remove gemfiles
    
    * Refactor sites
    
    * Format article sites urls
    
    * Add liquid code to index
    
    * Add formatting to date
    
    * Add iteration from data
    
    * Add favicon
    
    * Change additon
    
    * Refactor iteration
    
    * Assing i in for loop
    
    * Assing i in for loop
    
    * Assing i in for loop
    
    * Assing i in for loop
    
    * Add python script for filtering
    
    * add logic to filter
    
    * Add iteration to index.md
    
    * Refactor structure and filter

--- filter_data.py
+++ filter_data.py
@@ -6,15 +6,21 @@ def remove_past_sundays(date):
     sunday = datetime.fromisoformat(date)
     now = datetime.now()
     if sunday > now:
-        return True
+        return sunday
     else:
+        return None
+
+
+def not_none(element):
+    if element is None:
         return False
+    return True
 
 
-filtered_dates = []
-with open('./_data/shopping-sundays.csv', 'r') as file:
+mapped_dates = []
+with open('jekyll/_data/shopping-sundays.csv', 'r') as file:
     for dates in csv.reader(file):
-        filtered_dates = filter(remove_past_sundays, dates)
+        mapped_dates = map(remove_past_sundays, dates)
 
-with open('./_data/filtered-shopping-sundays.csv', 'w') as file:
-    csv.writer(file).writerow(filtered_dates)
+with open('jekyll/_data/filtered-shopping-sundays.csv', 'w') as file:
+    filtered_dates = filter(not_none, mapped_dates)
