38b0be171b686e84d924497b8064a1cb05c123ad
skypaw <53307746+skypaw@users.noreply.github.com>
2023-11-01 21:34:05 +0100

    Add filter for dates

--- filter_data.py
+++ filter_data.py
@@ -6,15 +6,14 @@ def remove_past_sundays(date):
     sunday = datetime.fromisoformat(date)
     now = datetime.now()
     if sunday > now:
-        return sunday
-    else:
-        return None
+        return str(sunday)
 
 
 def not_none(element):
-    if element is None:
+    if not element:
         return False
-    return True
+    else:
+        return True
 
 
 mapped_dates = []
@@ -22,5 +21,9 @@ with open('jekyll/_data/shopping-sundays.csv', 'r') as file:
     for dates in csv.reader(file):
         mapped_dates = map(remove_past_sundays, dates)
 
-with open('jekyll/_data/filtered-shopping-sundays.csv', 'w') as file:
-    filtered_dates = filter(not_none, mapped_dates)
+with open('jekyll/_data/filtered-shopping-sundays.csv', 'w', newline='') as file:
+    filtered_dates = filter(not_none, list(mapped_dates))
+    writer = csv.DictWriter(file, fieldnames=['dates'])
+
+    writer.writeheader()
+    writer.writerows([{'dates': date} for date in filtered_dates])
