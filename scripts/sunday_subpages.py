1dd95670e8b2b144b57bfcc32ceccde46cb13a32
skypaw <53307746+skypaw@users.noreply.github.com>
2023-12-26 16:40:03 +0100

    Reformat sunday_subages

--- scripts/sunday_subpages.py
+++ scripts/sunday_subpages.py
@@ -16,28 +16,30 @@ class AnySunday:
         return f'czy-{self.date.strftime("%d-%m-%Y")}-jest-niedziela-handlowa.md'
 
     def sitemap(self):
-        return {'loc': f'https://czyjesthandlowa.pl/{self.sunday_url()}/',
-                'changefreq': 'monthly'}
+        return {
+            "loc": f"https://czyjesthandlowa.pl/{self.sunday_url()}/",
+            "changefreq": "monthly",
+        }
 
     def template(self, shops_open) -> str:
         return any_sunday_template(self.jekyll_date, shops_open)
 
     async def generate(self, shops_open):
-        async with aiofiles.open(self.sunday_url(), mode='w') as md:
+        async with aiofiles.open(self.sunday_url(), mode="w") as md:
             await md.writelines(self.template(shops_open))
         await self.build_sitemap()
 
     async def build_sitemap(self):
-        async with aiofiles.open('jekyll/sitemap.xml', 'r') as sitemap:
+        async with aiofiles.open("jekyll/sitemap.xml", "r") as sitemap:
             parsed_sitemap = xmltodict.parse(await sitemap.read())
-        with aiofiles.open('jekyll/sitemap.xml', 'w') as sitemap:
-            parsed_sitemap['urlset']['url'].append(self.sitemap())
+        with aiofiles.open("jekyll/sitemap.xml", "w") as sitemap:
+            parsed_sitemap["urlset"]["url"].append(self.sitemap())
             xmltodict.unparse(parsed_sitemap, sitemap)
 
 
 class ClosestSunday(AnySunday):
     def sunday_url(self) -> str:
-        return f'czy-najblizsza-niedziela-jest-handlowa.md'
+        return "czy-najblizsza-niedziela-jest-handlowa.md"
 
     def template(self, shops_open) -> str:
         return closest_sunday_template(self.date, self.jekyll_date)
@@ -69,11 +71,11 @@ class Config:
 
     @staticmethod
     def load_config():
-        with open('jekyll/_data/filtered-shopping-sundays.json') as config:
-            return json.load(config)['dates']
+        with open("jekyll/_data/filtered-shopping-sundays.json") as json_config:
+            return json.load(json_config)["dates"]
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     config = Config()
 
     for date in config.dates():
